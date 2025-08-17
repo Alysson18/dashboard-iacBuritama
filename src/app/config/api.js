832cba2de96988edc066aa426ee0a1f1385218b4
Alysson <alysson.paulino@hotmail.com>
2025-08-17 17:16:25 -0300

    first commit

--- src/app/config/api.js
+++ src/app/config/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
 
-    baseURL: "http://127.0.0.1:3001",
-    //baseURL: "https://checkin-api.iacburitama.com.br/api",
+    //baseURL: "http://127.0.0.1:3001",
+    baseURL: "https://checkin-api.iacburitama.com.br/api",
     timeout: 12000,
     auth: {
         username: 'Alysson',  //process.env.REACT_APP_USUARIO,
