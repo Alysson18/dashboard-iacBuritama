3fc5a155fcb2371e34fdf72b752e1725f6c4dd52
Alysson <alysson.paulino@hotmail.com>
2025-09-20 15:33:58 -0300

    Ajuste API

--- src/app/config/api.js
+++ src/app/config/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
 
     //baseURL: "http://127.0.0.1:3001",
-    baseURL: "https://checkin-api.iacburitama.com.br/api",
+    baseURL: "https://checkin-api.iacburitama.com.br",
     timeout: 12000,
     auth: {
         username: 'Alysson',  //process.env.REACT_APP_USUARIO,
